46f48c1f8ccfd6352e32d02a3a331581b9218644
ra9_adept <gerasimov.ra9@gmail.com>
2024-03-17 16:13:07 +0400

    Adding selenoid authorization

--- tests/conftest.py
+++ tests/conftest.py
@@ -17,7 +17,7 @@ def setup_browser():
     }
     options.capabilities.update(capabilities)
     driver = webdriver.Remote(
-        command_executor="https://selenoid.autotests.cloud/wd/hub",
+        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
         options=options
     )
 
