d5d505d014bfc1183720a6741cde4f7f88c3a05c
ra9_adept <gerasimov.ra9@gmail.com>
2024-03-17 16:21:41 +0400

    Trying to fix selenoid browser config

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,37 +1,36 @@
 import pytest
 import allure
-from selene import browser, Browser, Config
+from selene import browser
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 
 
 @pytest.fixture(scope="function", autouse=True)
 def setup_browser():
-    options = Options()
-    capabilities = {
-        "browserName": "firefox",
-        "browserVersion": "123.0",
-        "selenoid:options": {
-            "enableVideo": True
-        }
-    }
-    options.capabilities.update(capabilities)
-    driver = webdriver.Remote(
-        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
-        options=options
-    )
-
-    browser = Browser(Config(driver))
-
     with allure.step("Settings base url"):
         browser.config.base_url = "https://demoqa.com"
-    with allure.step("Selecting Firefox for browser"):
-        browser.config.driver_name = "firefox"
     with allure.step("Setting up timeout for browser"):
         browser.config.timeout = 6.0
     with allure.step("Setting up browser window size"):
         browser.config.window_width = 1920
         browser.config.window_height = 1200
+
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "firefox",
+        "browserVersion": "123.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options)
+    browser.config.driver = driver
+
     yield
+
     browser.quit()
 
