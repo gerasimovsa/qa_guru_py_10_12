43e69aa856fc500d712734598691ee487d422ee8
ra9_adept <gerasimov.ra9@gmail.com>
2024-03-17 16:35:31 +0400

    Adding allure attachments

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,7 @@ import allure
 from selene import browser
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
+from utils import attach
 
 
 @pytest.fixture(scope="function", autouse=True)
@@ -14,23 +15,28 @@ def setup_browser():
     with allure.step("Setting up browser window size"):
         browser.config.window_width = 1920
         browser.config.window_height = 1200
-
-    options = Options()
-    selenoid_capabilities = {
-        "browserName": "firefox",
-        "browserVersion": "123.0",
-        "selenoid:options": {
-            "enableVNC": True,
-            "enableVideo": True
+    with allure.step("Setting up Selenoid remote executor"):
+        options = Options()
+        selenoid_capabilities = {
+            "browserName": "firefox",
+            "browserVersion": "123.0",
+            "selenoid:options": {
+                "enableVNC": True,
+                "enableVideo": True
+            }
         }
-    }
-    options.capabilities.update(selenoid_capabilities)
-    driver = webdriver.Remote(
-        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
-        options=options)
-    browser.config.driver = driver
+        options.capabilities.update(selenoid_capabilities)
+        driver = webdriver.Remote(
+            command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
+            options=options)
+        browser.config.driver = driver
 
     yield
+
+    attach.add_html(browser)
+    attach.add_logs(browser)
+    attach.add_screenshot(browser)
+    attach.add_video(browser)
 
     browser.quit()
 
